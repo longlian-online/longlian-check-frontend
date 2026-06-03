9d87935c350515c9cfaf57534ee7fe81ce88defa
asukai <3474790137@qq.com>
2026-06-04 02:02:49 +0800

    init

---
+++ src/components/TextArea.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+    let { children } = $props()
+</script>
+<div class="card bg-base-300 rounded-box grid grow place-items-center">
+    {@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+	import TextArea from '../components/TextArea.svelte';
+</script>
+
+<div class="flex h-screen w-full">
+	<TextArea>content</TextArea>
+	<div class="divider divider-horizontal">OR</div>
+	<TextArea>content</TextArea>
+</div>
