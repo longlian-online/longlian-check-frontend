b96a139165eb459b5cb42ec9d9893485850b2eb1
asukai <3474790137@qq.com>
2026-06-05 00:14:10 +0800

    feat: 布局调整，添加标题

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
 	import '../app.css';
 	import { raw_text, check_text } from '$lib';
-	import { diffChars, diffWords } from 'diff';
+	import { diffWords } from 'diff';
 
 	type Change = { value: string; added?: boolean; removed?: boolean };
 
@@ -9,24 +9,29 @@
 	console.log(diffResult);
 </script>
 
-<div class="flex h-screen w-full">
-	<div class="bg-base-300 rounded-box grow place-items-start p-4">
-		{#each diffResult as part}
-			{#if part.removed}
-				<span class="bg-red-200 text-red-800 line-through rounded px-0.5">{part.value}</span>
-			{:else if !part.added}
-				<span>{part.value}</span>
-			{/if}
-		{/each}
+<div class="flex h-screen w-full flex-col">
+	<div class="flex-none p-4">
+		<h1 class="text-center text-2xl font-bold">Longlian Check</h1>
 	</div>
-	<div class="divider divider-horizontal">OR</div>
-	<div class="bg-base-300 rounded-box grow place-items-start p-4 overflow-auto">
-		{#each diffResult as part}
-			{#if part.added}
-				<span class="bg-green-200 text-green-800 rounded px-0.5">{part.value}</span>
-			{:else if !part.removed}
-				<span>{part.value}</span>
-			{/if}
-		{/each}
+	<div class="flex flex-1">
+		<div class="bg-base-300 rounded-box grow place-items-start p-4">
+			{#each diffResult as part}
+				{#if part.removed}
+					<span class="rounded bg-red-200 px-0.5 text-red-800 line-through">{part.value}</span>
+				{:else if !part.added}
+					<span>{part.value}</span>
+				{/if}
+			{/each}
+		</div>
+		<div class="divider divider-horizontal"></div>
+		<div class="bg-base-300 rounded-box grow place-items-start overflow-auto p-4">
+			{#each diffResult as part}
+				{#if part.added}
+					<span class="rounded bg-green-200 px-0.5 text-green-800">{part.value}</span>
+				{:else if !part.removed}
+					<span>{part.value}</span>
+				{/if}
+			{/each}
+		</div>
 	</div>
 </div>
